8806d951862eb5084be4f7cb86fd34bc1adc89cd
AnkitaArya-273303 <ankitaarya9695@gmail.com>
2026-01-15 15:32:06 +0530

    Add files via upload

---
+++ pi.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x=['mon','tues','wed','thur','fri','sat']
+y=[10,12,5,20,10,30]
+plt.plot(x,y)
+plt.title('Bakery sales')
+plt.xlabel('days of week')
+plt.ylabel('sales per day')
+plt.show()
